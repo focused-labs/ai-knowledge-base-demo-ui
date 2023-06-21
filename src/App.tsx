4806e6e00e2af8d6df735c24d1f34a838bca4cbc
Peter Swartwout <peter.swartwout@focusedlabs.io>
2023-06-21 08:52:33 -0400

    Centers the query form.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import {QueryForm} from "./components/QueryForm";
 function App() {
   return (
     <div className="App">
+        <div className="Welcome">
         <h1>
           Welcome to the Focused Labs Knowledge Hub
         </h1>
         <QueryForm />
+        </div>
     </div>
   );
 }
