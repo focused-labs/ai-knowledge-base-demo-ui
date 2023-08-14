b214356950510c29f770a26af7cd1731870f48ef
kaylynnwatson <kaylynnwatson@focusedlabs.io>
2023-08-14 18:03:17 -0400

    Remove tailwind, add color change on search bar with action

--- src/components/QueryForm.tsx
+++ src/components/QueryForm.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as SparkleIcon} from '../images/sparkleIcon.svg';
 import {ReactComponent as EnterButton} from '../images/enterButton.svg';
 import {
-    Grid,
     FormControl,
     Select,
     MenuItem,
     Typography,
     Button,
     TextField,
+    Box
 } from "@mui/material";
 import {commonColors} from "../styles/styles";
 
@@ -26,6 +26,8 @@ export const QueryForm: React.FC<{
           onSubmit
       }) => {
 
+    const [isFocused, setIsFocused] = useState(false);
+
     const roles = [
         {label: "No Preference", value: "none"},
         {label: "Potential Employee", value: "candidate"},
@@ -33,77 +35,74 @@ export const QueryForm: React.FC<{
     ]
 
     return (
-        <Grid container
-              sx={{width: 1}}
-              direction="column"
-              justifyContent="between">
-            <Grid>
-                <FormControl
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: 1,
+                border: `2px solid ${isFocused ? commonColors.purple600 : commonColors.lightGray}`,
+                borderRadius: 1,
+                transition: 'border 0.3s ease-in-out',
+                '&:hover': {
+                    border: `2px solid ${isFocused ? commonColors.purple600 : commonColors.purple400}`,
+                }
+            }}
+        >
+            <FormControl variant="outlined" sx={{marginRight: 1}}>
+                <Select
                     sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-start',
-                        padding: 1,
-                        border: `2px solid ${commonColors.lightGray}`,
-                        borderRadius: '0.375rem',
-                        height: "5rem"
-                    }}>
-                    <Select
-                        sx={{
-                            background: commonColors.purple100,
-                            color: commonColors.purple400,
-                            width: "20rem",
-                            '& .MuiInputBase-input': {
-                                display: 'flex',
-                            }
-                        }}
-                        value={persona}
-                        name="role"
-                        onChange={e => onPersonaChange(e.target.value)}>
-                        {
-                            roles.map((role, i) => (
-                                <MenuItem key={i} value={role.value}>
-                                    <SparkleIcon style={{
-                                        color: commonColors.purple600,
-                                        margin: 0,
-                                    }}/>
-                                    <Typography sx={{ml: 2, color: commonColors.purple600}}>
-                                        {role.label}
-                                    </Typography>
-
-                                </MenuItem>
-                            ))
+                        background: commonColors.purple100,
+                        color: commonColors.purple400,
+                        width: "20rem",
+                        '& .MuiInputBase-input': {
+                            display: 'flex',
                         }
-                    </Select>
-
-                    <TextField
-                        variant="outlined"
-                        placeholder="Ask Something"
-                        fullWidth={true}
-                        value={inputQuery}
-                        onChange={e => onInputQueryChange(e.target.value)}
-                        sx={{
-                            marginLeft: 2,
-                            height: 1,
-                            "& fieldset": {border: 'none'},
-                        }}
-                        onKeyDown={e => {
-                            if (e.key === 'Enter')
-                                onSubmit()
-                        }}
-                    />
-                    <Button
-                        type="submit"
-                        className="border-1 rounded-full"
-                        onClick={e => {
-                            e.preventDefault();
-                            onSubmit()
-                        }}
-                    >
-                        <EnterButton className="mx-2"/>
-                    </Button>
-                </FormControl>
-            </Grid>
-        </Grid>
-    )
-}
+                    }}
+                    value={persona}
+                    name="role"
+                    onChange={e => onPersonaChange(e.target.value)}>
+                    {
+                        roles.map((role, i) => (
+                            <MenuItem key={i} value={role.value}>
+                                <SparkleIcon style={{
+                                    color: commonColors.purple600,
+                                    margin: 0,
+                                }}/>
+                                <Typography sx={{ml: 2, color: commonColors.purple600}}>
+                                    {role.label}
+                                </Typography>
+                            </MenuItem>
+                        ))
+                    }
+                </Select>
+            </FormControl>
+            <TextField
+                // variant="outlined"
+                placeholder="Ask Something"
+                sx={{
+                    flex: 1,
+                    marginRight: 1,
+                    "& fieldset": {border: 'none'},
+                }}
+                onClick={() => {
+                    console.log("pressed")
+                    setIsFocused(true)
+                }}
+                onBlur={() => {
+                    console.log("unpressed")
+                    setIsFocused(false)
+                }}
+            />
+            <Button
+                type="submit"
+                className="border-1 rounded-full"
+                onClick={e => {
+                    e.preventDefault();
+                    onSubmit()
+                }}
+            >
+                <EnterButton className="mx-2"/>
+            </Button>
+        </Box>
+    );
+};
